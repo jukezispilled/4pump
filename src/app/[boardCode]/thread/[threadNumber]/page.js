2465cc5b4c03c209d466cdb1af3b9977ecd7917c
agorapilled <johngardner13@outlook.com>
2025-08-12 23:58:03 -0400

    v0

--- src/app/[boardCode]/thread/[threadNumber]/page.js
+++ src/app/[boardCode]/thread/[threadNumber]/page.js
@@ -88,7 +88,7 @@ export default function ThreadPage({ params }) {
   return (
     <div className="max-w-6xl mx-auto p-4">
       <div className="mb-6">
-        <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center justify-between mb-6">
           <div className='absolute left-1/2 -translate-x-1/2 hidden md:block'>
             <h1 className="text-2xl font-bold text-[#890000]">
               /{boardCode}/ - {thread.subject || `Thread #${thread.threadNumber}`}
